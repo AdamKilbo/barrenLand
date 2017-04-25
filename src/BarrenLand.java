7896a84466c3e74718075fb0fb24fab383f855cf
AdamKilbo <adam.kilbo@target.com>
2017-04-25 10:11:37 -0500

    initial commit

---
+++ src/BarrenLand.java
@@ -0,0 +1,22 @@
+
+public class BarrenLand {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static void getInput() {
+		// read coords from stdin
+	}
+	
+	public static void parseInput(String input) {
+		// split string into parts by comma, then by spaces. Remove quotes and curly braces.
+		// store pieces as coords either in a string/int arrays in an string/int array
+	}
+	
+	public static void makeMap() {
+		// generate an int[400][600] map. 
+	}
+
+}
